d381f1c682ee1acec1c8205862b56857abc95d6f
Jennifer Beem <jenniferbeem@gmail.com>
2017-03-24 10:36:51 -0700

    add answer model

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,8 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  title: DS.attr(),
   content: DS.attr(),
-  addtl: DS.attr(),
-  answers: DS.hasMany('answer', { async: true })
+  question: DS.belongsTo('question', { async: true })
 });
