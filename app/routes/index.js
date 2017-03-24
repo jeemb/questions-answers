9499587eeefdb7f58ecbe3907e15d946eb20e56e
jb <jenniferbeem@gmail.com>
2017-03-23 19:41:11 -0700

    generate question model and add hardcoded data to test display

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  author: "Mary",
+  title: "What is dihydrogen monoxide?",
+  content: "HELP",
+  addtl: "extra extra"
+}];
+
 export default Ember.Route.extend({
   model() {
     return questions;
