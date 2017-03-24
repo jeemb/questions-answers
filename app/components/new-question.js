3ddfa23351c3c4696dd6d8c1f61f04dccea4991e
Jennifer Beem <jenniferbeem@gmail.com>
2017-03-24 09:39:52 -0700

    add new-question component and actions

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    addNewQuestion: false,
+    showAddQuestionForm() {
+      this.set("addNewQuestion", true);
+    },
+    saveNewQuestion() {
+      var params = {
+        author: this.get("author"),
+        title: this.get("title"),
+        content: this.get("content"),
+        addtl: this.get("addtl")
+      };
+      this.set("addNewQuestion", false);
+      this.sendAction("saveNewQuestion", params);
+      )
+    }
+  }
 });
